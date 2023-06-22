cb4c0edad2ef740e77b28a6ecfd5778c8489f58d
yuta6686 <yuta20010614@gmail.com>
2023-06-22 18:56:02 +0900

    play_ground 色々追加

--- play_ground/src/main.rs
+++ play_ground/src/main.rs
@@ -1,5 +1,17 @@
 use std::string;
 
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -39,6 +51,8 @@ fn main() {
         println!("{}",i);
     }
 
+    // String型は動的メモリで取得している
+    // Rustはスマートポインタの仕様を使ってる
     let mut s = String::from("hello");
     let cs = "hello";
     println!("{}",s);
@@ -47,11 +61,99 @@ fn main() {
 
     println!("{}",s);
 
+    let s1 = String::from("hello");
 
+    
+
+    let s2 = s1.clone();
+
+    println!("{}, world!", s1);
+
+    takes_ownership(s1);    
+    
+    let x = 5;
+    makes_copy(x);
+
+    // String::from();と一緒
+    let mut s1 = gives_ownership();
+    
+    let s2 = String::from("hello");
+
+    // s2の所有権を関数に入れてそれを返している=所有権はs2->s3に移った。
+    let s3 = takes_and_gives_back(s2);
+
+    let a = calculate_length(&s1);
+    println!("{}",a);
+
+    change(&mut s1);
+
+    let mut ss =  String::from("test fawe");
+
+    println!("{}",first_word(&ss)) ;
+
+    s.clear();
+
+    let rect1 = Rectangle{width:30,height:50};
+    println!("rect1 is {:#?}",rect1);
+
+    ShowRectangle(&rect1);
+    
+    println!("{}",rect1.area());
 }
 
 fn test_function (x: i32) -> i32
 {
     println!("test {}",x);
     x
+}
+
+fn takes_ownership(some_string: String)
+{
+    println!("{}",some_string);
+}
+
+fn makes_copy(some_integer: i32)
+{
+    println!("{}",some_integer);
+}
+
+fn gives_ownership() -> String{
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String)->String
+{
+    a_string
+}
+
+// 参照にすれば、メモリ解放までは行われない。
+// その中身をいじれる
+fn calculate_length(s:&String) -> usize
+{
+    s.len()
+}
+
+
+fn change(some_string: &mut String)
+{
+    some_string.push_str(", world");
+}
+
+fn first_word(s: &String) -> &str{
+    let bytes = s.as_bytes();
+
+    for(i,&item) in bytes.iter().enumerate()
+    {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn ShowRectangle(rect :&Rectangle)
+{
+    println!("{:#?}",rect);
 }
